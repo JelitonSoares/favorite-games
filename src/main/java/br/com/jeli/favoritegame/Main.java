35e8d8616145d55b2b849a3f80b48d6dbcde7c30
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-02-21 14:07:52 -0300

    Construindo o projeto

---
+++ src/main/java/br/com/jeli/favoritegame/Main.java
@@ -0,0 +1,7 @@
+package br.com.jeli.favoritegame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciando projeto");
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $jeli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
