c85a868d80fe558aaa2dd4e54d0d8a2b2ecd5262
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-02-24 12:16:51 -0300

    'Criando a classe responsáel por interagir e realiar as operações de um usuario'

---
+++ src/main/java/br/com/jeli/favoritegame/domain/usuario/UsuarioService.java
@@ -0,0 +1,11 @@
+package br.com.jeli.favoritegame.domain.usuario;
+
+public class UsuarioService {
+    public Boolean entrar(String id, String senha) {
+        return true;
+    }
+
+    public void cadastrar(String id, String nome, String senha){
+
+    }
+}
