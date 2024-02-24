55427c13a6699f7f9d2a9ac52cbb11a102a6a770
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-02-24 12:15:46 -0300

    'Atualizando as opções do menu'

--- src/main/java/br/com/jeli/favoritegame/FavoriteGamesApplication.java
+++ src/main/java/br/com/jeli/favoritegame/FavoriteGamesApplication.java
@@ -1,9 +1,12 @@
 package br.com.jeli.favoritegame;
 
+import br.com.jeli.favoritegame.domain.usuario.UsuarioService;
+
 import java.util.Scanner;
 
 public class FavoriteGamesApplication {
 
+    private static UsuarioService service = new UsuarioService();
     private static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
     public static void main(String[] args) {
         Integer opcao = exibirMenu();
@@ -27,7 +30,7 @@ public class FavoriteGamesApplication {
         String menu = """
                 ------Bem vindo ao FavoriteGames------
                 1 - Entrar
-                2 - Cadastrar conta
+                2 - Cadastrar usuario
                 3 - Sair
                 """;
         System.out.println(menu);
@@ -35,9 +38,20 @@ public class FavoriteGamesApplication {
     }
 
     private static void entrar(){
-        System.out.println("Entrando");
+        System.out.println("Digite seu ID: ");
+        String id = scanner.next();
+
+        System.out.println("Digite sua senha: ");
+        String senha = scanner.next();
+
+        Boolean entrou = service.entrar(id, senha);
+        menuEntrar(entrou);
         System.out.println("Pressione qualquer tecla para voltar para o menu");
         scanner.next();
+    }
+
+    private static void menuEntrar(Boolean entrou) {
+
     }
 
     private static void cadastrar(){
