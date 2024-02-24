a0f7d6b653afe6bdbbc12a89b68a8ad248f97bd4
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-02-24 11:47:04 -0300

    'Criando o menu de entrar/cadastrar'

--- src/main/java/br/com/jeli/favoritegame/FavoriteGamesApplication.java
+++ src/main/java/br/com/jeli/favoritegame/FavoriteGamesApplication.java
@@ -1,6 +1,49 @@
 package br.com.jeli.favoritegame;
 
+import java.util.Scanner;
+
 public class FavoriteGamesApplication {
-    public static void main(String[] args) {}
+
+    private static Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+    public static void main(String[] args) {
+        Integer opcao = exibirMenu();
+        while(opcao != 3) {
+            switch (opcao){
+                case 1:
+                    entrar();
+                    break;
+                case 2:
+                    cadastrar();
+                    break;
+            }
+            opcao = exibirMenu();
+        }
+
+        System.out.println("Obrigado por usar o FavoriteGames! Volte sempre:)");
+        System.out.println("Finalizando...");
+    }
+
+    private static int exibirMenu() {
+        String menu = """
+                ------Bem vindo ao FavoriteGames------
+                1 - Entrar
+                2 - Cadastrar conta
+                3 - Sair
+                """;
+        System.out.println(menu);
+        return scanner.nextInt();
+    }
+
+    private static void entrar(){
+        System.out.println("Entrando");
+        System.out.println("Pressione qualquer tecla para voltar para o menu");
+        scanner.next();
+    }
+
+    private static void cadastrar(){
+        System.out.println("Cadastrando");
+        System.out.println("Pressione qualquer tecla para voltar para o menu");
+        scanner.next();
+    }
 
 }
