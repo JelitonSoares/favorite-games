4d37f8e289a851b94bd2a273602ed3954db59f61
Jeliton Soares <jelitonn.soarres@gmail.com>
2024-02-25 09:12:36 -0300

    'Adicionando novas funcionalidades no menu'

--- src/main/java/br/com/jeli/favoritegame/FavoriteGamesApplication.java
+++ src/main/java/br/com/jeli/favoritegame/FavoriteGamesApplication.java
@@ -51,11 +51,76 @@ public class FavoriteGamesApplication {
     }
 
     private static void menuEntrar(Boolean entrou) {
+        System.out.println("*-*-*-*-*-*-*-Bem vindo Fulando*-*-*-*-*-*-*-");
+        System.out.println("Pressione enter para ir ao menu principal");
+        scanner.next();
+        while(entrou){
+            Integer opcao = exibirMenuEntrar();
+            switch (opcao) {
+                case 1:
+                    listarJogos();
+                    break;
+                case 2:
+                    cadastrarJogos();
+                    break;
+                case 3:
+                    excluirJogos();
+                    break;
+                case 4:
+                    atualizarJogos();
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    entrou = false;
+                    break;
+            }
+
+        }
+
+    }
+
+    private static int exibirMenuEntrar() {
+        String menu = """
+                1 - Listar jogos favoritos
+                2 - Cadastrar jogos favoritos
+                3 - Excluir jogo favorito
+                4 - Atualizar jogo favorito
+                5 - Sair
+                """;
+
+        System.out.println(menu);
+        return scanner.nextInt();
+    }
+
+    private static void listarJogos() {
+
+    }
+
+    private static void cadastrarJogos(){
+
+    }
+
+    private static void excluirJogos(){
+
+    }
+
+    private static void atualizarJogos(){
 
     }
 
     private static void cadastrar(){
-        System.out.println("Cadastrando");
+        System.out.println("Digite o ID da seu usuario: (Ele deve conter 5 digitos): ");
+        String id = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite sua senha: (Ela deve conter 8 dígitos): ");
+        String senha = scanner.next();
+
+        service.cadastrar(id, nome, senha);
+
+        System.out.println("Cadastro realizado com sucesso!!");
         System.out.println("Pressione qualquer tecla para voltar para o menu");
         scanner.next();
     }
